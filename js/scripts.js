5cb8de6911c3e64c26212111bfc14cfad3f6f6be
Vohsty <stevekimanthi87@gmail.com>
2019-04-08 10:12:16 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+//BUSINESS LOGIC
 $().ready(()=>{
   var name = 0;
+  var name2 =0;
   var rollScoreOne = [];
   var rollScoreTwo = [];
   var fav = [];
@@ -24,11 +26,11 @@ $().ready(()=>{
         grayer('form2',false);
         grayer('form1',true);
       }
+
       else {
         subTotal = subTotal +randomNo1;
         $(".p1Out").text(randomNo1);
         $(".output").text(subTotal);
-
       }
 
     });
@@ -52,13 +54,9 @@ $().ready(()=>{
           $(".p2Out").text(randomNo2);
           $(".output").text(subTotal2);
         }
-
-
-
     });
 
     $("#hold").click(()=>{
-
        total = total+ subTotal;
        $(".output2").text("Your score Is " +total);
        subTotal=0;
@@ -66,15 +64,16 @@ $().ready(()=>{
        $(".p1Out").text(subTotal);
        grayer('form1',true);
        grayer('form2',false);
-
-       if (total>99){
-         alert("player one worn");
+       if (total>20){
+         alert(name +" worn");
          total =100;
          $(".output2").text("Your score Is " +total);
+         grayer('form2',true);
+         $(".winner").fadeIn();
+          $(".win").text(name);
        }
-
-
     });
+
     $("#hold2").click(()=>{
 
        total2 = total2+ subTotal2;
@@ -85,10 +84,13 @@ $().ready(()=>{
        grayer('form1',false);
        grayer('form2',true);
 
-       if (total2>99) {
-         alert("player 2 worn");
+       if (total2>20) {
+         alert(name2+" worn");
          total2=100;
          $(".output3").text("Your score Is " +total2);
+         grayer('form1',true);
+         $(".winner").fadeIn();
+         $(".win").text(name2);
        }
 
      });
@@ -96,13 +98,36 @@ $().ready(()=>{
 
 
     function grayer(formId, yesNo) {
-   var f = document.getElementById(formId), s, opacity;
-   s = f.style;
-   opacity = yesNo? '40' : '100';
-   s.opacity = s.MozOpacity = s.KhtmlOpacity = opacity/100;
-   s.filter = 'alpha(opacity='+opacity+')';
-   for(var i=0; i<f.length; i++) f[i].disabled = yesNo;
-  }
+     var f = document.getElementById(formId), s, opacity;
+     s = f.style;
+     opacity = yesNo? '40' : '100';
+     s.opacity = s.MozOpacity = s.KhtmlOpacity = opacity/100;
+     s.filter = 'alpha(opacity='+opacity+')';
+     for(var i=0; i<f.length; i++) f[i].disabled = yesNo;
+    }
 
+  function PlayersInfo(name, score, totalScore) {
+      this.playerNames = name;
+      this.playerScore = score;
+      this.totalScores = totalScore;
+  }
+  //USER LOGIC
+  $("#play1").click(function(){
+  var player = $("#victim1").val();
+  name = player;
+  $(".playerNameOne").text(name);
+  $(".vic1").fadeOut();
+  $(".vic2").fadeIn();
+
+
+  });
+  $("#play2").click(function(){
+  var player2 = $("#victim2").val();
+  name2 = player2;
+  $(".playerNameTwo").text(name2);
+  $(".vic2").fadeOut();
+  $(".container").fadeIn();
+  });
+  
 
 });
